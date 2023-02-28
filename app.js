28098701e742e58071fdd28db4927d67f7ce83c5
Joao Carvalho <joao12carvalho@hotmail.com>
2023-02-28 02:19:40 -0300

    Reformulação do site

---
+++ app.js
@@ -0,0 +1,16 @@
+
+function clickBurguer() {
+    if( menu.style.display == 'block'){
+        menu.style.display='none'
+    }else{
+        menu.style.display = 'block'
+    }
+    function clickBurguer() {
+        const menu = document.getElementById("menu");
+        menu.classList.toggle("active");
+
+}
+}
+
+
+
