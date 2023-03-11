8b3b43f51eca8e4d49837d5e3a2ccfecc7a325f3
Joao Carvalho <joao12carvalho@hotmail.com>
2023-03-11 01:18:46 -0300

    instalando configurações google analytics

--- app.js
+++ app.js
@@ -35,7 +35,52 @@ curriculoBtn.addEventListener("click", () => {
 
 
 
+/*   ------------------------ CALCULANDO TAXA METABOLICA BASAL---------------- */
 
+// Obtém a janela modal
+var modalTmb = document.getElementById("myModalTmb");
 
+// Obtém o botão que abre a janela modal
+var btnTmb = document.getElementById("myBtnTmb");
 
+// Obtém o elemento <span> que fecha a janela modal
+var spanTmb = document.getElementsByClassName("closeTmb")[0];
 
+// Quando o usuário clicar no botão, abre a janela modal
+btnTmb.onclick = function() {
+  modalTmb.style.display = "block";
+}
+
+// Quando o usuário clicar no <span> (x), fecha a janela modal
+spanTmb.onclick = function() {
+  modalTmb.style.display = "none";
+}
+
+// Quando o usuário clicar fora da janela modal, fecha-a
+window.onclick = function(event) {
+  if (event.target == modalTmb) {
+    modalTmb.style.display = "none";
+  }
+}
+
+// Quando o usuário envia o formulário, calcula a TMB e exibe o resultado
+document.getElementById("myFormTmb").addEventListener("submit", function(event) {
+  event.preventDefault(); // previne o envio padrão do formulário
+  var gender = document.getElementById("gender").value;
+  var weight = parseFloat(document.getElementById("weight").value);
+  var height = parseFloat(document.getElementById("height").value);
+  var age = parseInt(document.getElementById("age").value);
+
+  if (!gender || !weight || !height || !age) {
+    alert("Por favor, preencha todos os campos.");
+    return;
+    }
+    
+    if (gender === "male") {
+    var tmb = 88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age);
+    } else if (gender === "female") {
+    var tmb = 447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age);
+    }
+    
+    document.getElementById("result").innerHTML = "Sua Taxa Metabólica Basal é " + tmb.toFixed(2) + " kcal/dia.";
+    });
